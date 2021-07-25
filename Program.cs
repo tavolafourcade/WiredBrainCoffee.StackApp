ce917e6d85149358e7ddfe6c15b6fe605dd9db97
tavolafourcade <tavolafourcade@gmail.com>
2021-07-24 23:30:13 -0500

    Using Push and Pop methods

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace WiredBrainCoffee.StackApp
             stack.Push(1.2); // Using Ctrl + . we can generate the method Push on the class SimpleStack
             stack.Push(2.8);
             stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while(stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+            Console.WriteLine($"Sum: {sum}");
+            Console.ReadLine();
         }
     }
 }
