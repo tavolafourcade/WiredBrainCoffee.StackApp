9943067307a1153cc431dd2d13deafb080639b73
tavolafourcade <tavolafourcade@gmail.com>
2021-07-24 23:03:34 -0500

    1. Added an index for the stack 2. Specified a max items of 10 to SimpleStack 3. Write the increment operator before _currentIndex in order to increase from -1 to 0 when the Push method is called

--- SimpleStack.cs
+++ SimpleStack.cs
@@ -4,13 +4,21 @@ namespace WiredBrainCoffee.StackApp
 {
     public class SimpleStack
     {
+        private readonly double[] _items;
+        private int _currentIndex = -1; // If no item is in SimpleStack, the current index is -1.
+
         public SimpleStack()
         {
+            _items = new double[10]; // This means the SimpleStack class will work with a max of 10 items.
         }
 
-        public void Push(double v)
+        public void Push(double item)
         {
-            throw new NotImplementedException();
+            /* Write the increment operator before _currentIndex
+             * In this way, it will first increment from -1 to 0
+             * Then, it will be assign to _items when the Push method is called.
+             */
+            _items[++_currentIndex] = item; 
         }
     }
 }
