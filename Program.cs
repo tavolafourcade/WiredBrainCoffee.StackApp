e9e89f26949ead4bd96a61d88202c257050341f1
tavolafourcade <tavolafourcade@gmail.com>
2021-07-25 15:03:08 -0500

    1. Implemented a StackStrings method to stack strings 2. Discovered the SimpleStack method only works with Double so far

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ namespace WiredBrainCoffee.StackApp
     {
         static void Main(string[] args)
         {
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
+        }
+
+
+
+        private static void StackDoubles()
+        {
             var stack = new SimpleStack(); //Initialize an instance
             stack.Push(1.2); // Using Ctrl + . we can generate the method Push on the class SimpleStack
             stack.Push(2.8);
@@ -13,14 +22,20 @@ namespace WiredBrainCoffee.StackApp
 
             double sum = 0.0;
 
-            while(stack.Count > 0)
+            while (stack.Count > 0)
             {
                 double item = stack.Pop();
                 Console.WriteLine($"Item: {item}");
                 sum += item;
             }
             Console.WriteLine($"Sum: {sum}");
-            Console.ReadLine();
+        }
+
+        private static void StackStrings()
+        {
+            var stack = new SimpleStack();
+            stack.Push("Wired Brain Coffee");
+            stack.Push("Pluralsight");
         }
     }
 }
