ff6e00a835d90f5e276beee0584526f8e83945b9
tavolafourcade <tavolafourcade@gmail.com>
2021-07-24 22:43:32 -0500

    Initial repo of the Project case: Wired Brain Scenario needs a .NET console application to load and save employees and organization that visit their coffee shops

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
